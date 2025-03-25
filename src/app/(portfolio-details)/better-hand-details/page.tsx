0c73507855e6629df8287e449777e7633ce8a576
mahfuj926 <mahfujrahman.jewel@gmail.com>
2025-03-25 19:18:42 +0600

    made some changes

---
+++ src/app/(portfolio-details)/better-hand-details/page.tsx
@@ -0,0 +1,12 @@
+import BetterHandDetailsMain from '@/pages/portfolio/details/better-hand-details-main';
+import React from 'react';
+
+const BetterHandDetailsPage = () => {
+    return (
+        <div>
+            <BetterHandDetailsMain />
+        </div>
+    );
+};
+
+export default BetterHandDetailsPage;
