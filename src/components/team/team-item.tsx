e1c97ff8cc9e06c93972e3a9d7b7360a9c497afc
mahfuj926 <mahfujrahman.jewel@gmail.com>
2025-05-03 18:39:58 +0600

    remove the image from each team

--- src/components/team/team-item.tsx
+++ src/components/team/team-item.tsx
@@ -1,28 +1,51 @@
+// import React from "react";
+// import Image from "next/image";
+// import { ITeamDT } from "@/types/team-d-t";
+
+// // prop type 
+// type IProps = {
+//   item:ITeamDT;
+//   handleTeamModal(team: ITeamDT): void;
+// }
+
+// export default function TeamItem({ item,handleTeamModal }:IProps) {
+//   return (
+//     <div className="tp-team-item tp-hover-btn-wrapper marque fix mb-30">
+//       <div className="tp-hover-btn-item">
+//         <Image src={item.image} alt="team-img" width={375} height={464} />
+//       </div>
+//       <div className="tp-team-content">
+//         <span style={{fontFamily: 'Glacial Indifference', fontWeight: 'bold'}}>{item.designation}</span>
+//         <h4
+//           className="tp-team-title-sm"
+//           onClick={()=> handleTeamModal(item)}
+//         >
+//           <a href="#" style={{fontFamily: 'Glacial Indifference', fontWeight: 'bold'}}>{item.name}</a>
+//         </h4>
+//       </div>
+//     </div>
+//   );
+// }
+
 import React from "react";
-import Image from "next/image";
 import { ITeamDT } from "@/types/team-d-t";
 
 // prop type 
 type IProps = {
-  item:ITeamDT;
+  item: ITeamDT;
   handleTeamModal(team: ITeamDT): void;
-}
+};
 
-export default function TeamItem({ item,handleTeamModal }:IProps) {
+export default function TeamItem({ item, handleTeamModal }: IProps) {
   return (
-    <div className="tp-team-item tp-hover-btn-wrapper marque fix mb-30">
-      <div className="tp-hover-btn-item">
-        <Image src={item.image} alt="team-img" width={375} height={464} />
-      </div>
-      <div className="tp-team-content">
-        <span style={{fontFamily: 'Glacial Indifference', fontWeight: 'bold'}}>{item.designation}</span>
-        <h4
-          className="tp-team-title-sm"
-          onClick={()=> handleTeamModal(item)}
-        >
-          <a href="#" style={{fontFamily: 'Glacial Indifference', fontWeight: 'bold'}}>{item.name}</a>
-        </h4>
-      </div>
+    <div
+      className="bg-white border border-gray-200 p-4 rounded-md text-center"
+      // onClick={() => handleTeamModal(item)}
+    >
+      <h4 className="text-lg font-semibold text-gray-800 mb-1">
+        {item.name}
+      </h4>
+      <p className="text-sm text-gray-600">{item.designation}</p>
     </div>
   );
 }
